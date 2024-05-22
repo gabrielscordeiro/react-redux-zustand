15400325a6067527fe986ffa32bea64ebe973f38
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-05-21 23:05:58 -0300

    feat: 🎸 Loading courses data

--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -2,27 +2,52 @@ import { createSlice,PayloadAction } from '@reduxjs/toolkit'
 
 import { useAppSelector } from '..'
 
+
+interface Course {
+    id: number
+    modules: Array<{
+        id: number
+        title: string
+        lessons: Array<{
+            id: string
+            title: string
+            duration: string
+        }>
+    }>
+}
+
+export interface PlayerState {
+    course: Course | null;
+    currentModuleIndex: number;
+    currentLessonIndex: number;
+}
+
+const initialState: PlayerState = {
+    course: null,
+    currentModuleIndex: 0,
+    currentLessonIndex: 0,
+}
+
 export const playerSlice = createSlice({
     name: 'player',
-    initialState: {
-        course: ,
-        currentModuleIndex: 0,
-        currentLessonIndex: 0,
-    },
+    initialState,
     reducers: {
+        start: (state, action: PayloadAction<Course>) => {
+            state.course = action.payload
+        },
         play: (state, action: PayloadAction<[number, number]>) => {
             state.currentModuleIndex = action.payload[0]
             state.currentLessonIndex = action.payload[1]
         },
         next: (state) => {
             const nextLessonIndex = state.currentLessonIndex + 1
-            const nextLesson = state.course.modules[state.currentModuleIndex].lessons[nextLessonIndex]
+            const nextLesson = state.course?.modules[state.currentModuleIndex].lessons[nextLessonIndex]
 
             if (nextLesson) {
                 state.currentLessonIndex = nextLessonIndex
             } else {
                 const nextModuleIndex = state.currentModuleIndex + 1
-                const nextModule = state.course.modules[nextModuleIndex]
+                const nextModule = state.course?.modules[nextModuleIndex]
 
                 if (nextModule) {
                     state.currentModuleIndex = nextModuleIndex
@@ -34,14 +59,14 @@ export const playerSlice = createSlice({
 })
 
 export const player = playerSlice.reducer
-export const { play, next } = playerSlice.actions
+export const { play, next, start } = playerSlice.actions
 
 export const useCurrentLesson = () => {
     return useAppSelector(state => {
         const { currentModuleIndex, currentLessonIndex } = state.player
 
-        const currentModule = state.player.course.modules[currentModuleIndex]
-        const currentLesson = currentModule.lessons[currentLessonIndex]
+        const currentModule = state.player.course?.modules[currentModuleIndex]
+        const currentLesson = currentModule?.lessons[currentLessonIndex]
 
         return { currentModule, currentLesson }
     })
